8917a45084796efe3a9efb10a2e67fe7890bfd3b
teerapat <teerapatpor@gmai.com>
2021-02-27 21:49:51 +0700

    try to use .env

--- index.js
+++ index.js
@@ -13,13 +13,10 @@ const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 require("dotenv/config");
-mongoose.connect(
-  "mongodb+srv://admin:xQugQs8oZpNZEg8M@ownerproject.kpgm6.mongodb.net/ownerProject?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use("/uploads/", express.static("uploads"));
 app.use(cors({ credentials: true, origin: true }));
@@ -27,12 +24,8 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use("/api", routerAPI);
 
-require("./middlewares/socket")(
-  app,
-  io,
-  "mongodb+srv://admin:xQugQs8oZpNZEg8M@ownerproject.kpgm6.mongodb.net/ownerProject?retryWrites=true&w=majority"
-);
+require("./middlewares/socket")(app, io, process.env.DATABASE);
 // app.listen(port, () => {
 //   console.log("connect on port: ", port);
 // });
-server.listen(8888);
+server.listen(process.env.PORT || 8888);
